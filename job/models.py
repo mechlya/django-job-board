a372b5b627a08c99a122b284e9883f7ef6215456
mechlya <mechliamohamedamine@gmail.com>
2021-11-16 11:57:03 +0100

    job list - job detatils -add job

--- job/models.py
+++ job/models.py
@@ -1,6 +1,7 @@
 from django.contrib.admin.filters import ChoicesFieldListFilter
 from django.db import models
-
+from django.utils.text import slugify
+from django.contrib.auth.models import User
 # Create your models here.
 
 '''
@@ -23,7 +24,7 @@ def image_upload(instance, filename):
 
 class Job(models.Model): # Table 
 
-
+    owner = models.ForeignKey(User, related_name='job_owner', on_delete=models.CASCADE)
     title = models.CharField(max_length= 100)  # column
     job_type = models.CharField(max_length=15, choices = JOB_TYPE_CHOICES)
     description = models.TextField(max_length= 1000)
@@ -33,12 +34,31 @@ class Job(models.Model): # Table
     experence = models.IntegerField(default=1)
     category = models.ForeignKey('Category', on_delete=models.CASCADE)
     image = models.ImageField(upload_to=image_upload)
+    slug = models.SlugField(blank=True, null= True)
+
+    def save(self, *args, **kwargs):
+        self.slug = slugify(self.title)
+        super(Job,self).save(*args, **kwargs)
+
     def __str__(self):
         return self.title
 
 
 class Category( models.Model):
     name = models.CharField(max_length=25)
+
+    def __str__(self):
+        return self.name
+    
+
+class Apply(models.Model):
+    job = models.ForeignKey(Job, related_name='apply_job', on_delete=models.CASCADE)
+    name  = models.CharField(max_length=50)
+    email = models.EmailField(max_length=100)
+    website = models.URLField()
+    cv = models.FileField(upload_to='apply/')
+    cover_letter = models.TextField(max_length=500)
+    created_at =  models.DateTimeField(auto_now= True)
 
     def __str__(self):
         return self.name
